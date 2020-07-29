441bf31fa443830b3808d44822eb1d27e8b8bf12
rock harris <gkwkr@monsanto.com>
2020-07-29 15:09:48 -0500

    0001 - implemented the interface method as a stub - rock

--- cmd/chess-api/data-layer/repo.go
+++ cmd/chess-api/data-layer/repo.go
@@ -1,9 +1,19 @@
 package data
 
+import (
+	"errors"
+
+	data "github.com/number7/chess-api/cmd/chess-api/data-layer/structs"
+)
+
 type Repo struct {
 }
 
 // TODO: gonna need a client that talks to Postgres here
 func NewRepo() Repo {
 	return Repo{}
+}
+
+func (r Repo) Get(string) (data.GameData, error) {
+	return data.GameData{}, errors.New("not implemented")
 }
